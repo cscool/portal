03d34e537a3235854c15c4925668c4eba9a698c1
ernest <ernest@localhost.localdomain>
2014-05-01 22:26:37 -0700

    20140501-2026. clean out old code

--- README.md
+++
@@ -1,7 +0,0 @@
-portal
-======
-
-CS335 Portal-esk Game Attempt
-
-find the repository at https://github.com/cscool/portal
-
--- include/vector.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-using namespace std;
-
-float getAngle(Vec &p)
-{
-	 return (arctan(p[1]/p[0]));
-}
